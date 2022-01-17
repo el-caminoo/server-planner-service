35455131cae97d2bec94494ba6e8904fe52b13c9
Qalac <officialqalac@gmail.com>
2022-01-17 00:17:55 +0000

    finalised logic

--- src/server/server.service.ts
+++ src/server/server.service.ts
@@ -5,7 +5,33 @@ import {VirtualMachine, Server} from './server.interface';
 @Injectable()
 export class ServerService {
 
+    private resolve(serverType: Server, virtualMachines: Array<VirtualMachine>): number {
+        var currentState = serverType;
+        var count = 1;
+
+        for (let index = 0; index < virtualMachines.length; index++) {
+            const element = virtualMachines[index];
+
+            if (element["CPU"]>serverType["CPU"] || element["RAM"]>serverType["RAM"] || element["HDD"]>serverType["HDD"]) {
+                continue
+            }
+
+            else {
+                if ((currentState["CPU"]-element["CPU"]) >= 0 || (currentState["RAM"]-element["RAM"]) >= 0 || (currentState["HDD"]-element["HDD"]) >= 0) {
+                    var currentState = {"CPU":currentState["CPU"]-element["CPU"], "RAM":currentState["RAM"]-element["RAM"], "HDD":currentState["HDD"]-element["HDD"]}
+                }
+            
+                else if ((currentState["CPU"]-element["CPU"])<0 || (currentState["RAM"]-element["RAM"])<0 || (currentState["HDD"]-element["HDD"])<0){
+                    count += 1
+                    var currentState = serverType;
+                    var currentState = {"CPU":currentState["CPU"]-element["CPU"], "RAM":currentState["RAM"]-element["RAM"], "HDD":currentState["HDD"]-element["HDD"]}
+                }
+            }
+        }
+        return count;
+    }
+
     public calculate(serverType: Server, virtualMachines: Array<VirtualMachine>): number  {
-        return virtualMachines[0].RAM
+        return this.resolve(serverType, virtualMachines);
     }
 }
